719a4e422b0973d866ccbe8335165c42bdded7bf
Ify Osakwe <ifeakachukwuosakwe@gmail.com>
2025-09-06 20:30:21 +0100

    update converter

--- statistics-service/src/main/java/github/com/rexfilius/statisticsservice/repository/converter/DataPointIdReaderConverter.java
+++ statistics-service/src/main/java/github/com/rexfilius/statisticsservice/repository/converter/DataPointIdReaderConverter.java
@@ -1,15 +1,28 @@
 package github.com.rexfilius.statisticsservice.repository.converter;
 
-import com.mongodb.DBObject;
-
 import github.com.rexfilius.statisticsservice.model.timeseries.DataPointId;
-
+import org.bson.Document;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
+@ReadingConverter
+public class DataPointIdReaderConverter implements Converter<Document, DataPointId> {
+
+	@Override
+	public DataPointId convert(Document document) {
+		Date date = document.getDate("date");
+		String account = document.getString("account");
+		return new DataPointId(account, date);
+	}
+}
+
+
+/*
+ @Component
 public class DataPointIdReaderConverter implements Converter<DBObject, DataPointId> {
 
 	@Override
@@ -21,3 +34,4 @@ public class DataPointIdReaderConverter implements Converter<DBObject, DataPoint
 		return new DataPointId(account, date);
 	}
 }
+ */
--- statistics-service/src/main/java/github/com/rexfilius/statisticsservice/repository/converter/DataPointIdWriterConverter.java
+++ statistics-service/src/main/java/github/com/rexfilius/statisticsservice/repository/converter/DataPointIdWriterConverter.java
@@ -1,12 +1,24 @@
 package github.com.rexfilius.statisticsservice.repository.converter;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-
 import github.com.rexfilius.statisticsservice.model.timeseries.DataPointId;
-
+import org.bson.Document;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.stereotype.Component;
+
+@Component
+@WritingConverter
+public class DataPointIdWriterConverter implements Converter<DataPointId, Document> {
+
+	@Override
+	public Document convert(DataPointId id) {
+		return new Document()
+				.append("date", id.getDate())
+				.append("account", id.getAccount());
+	}
+}
+
+/*
 
 @Component
 public class DataPointIdWriterConverter implements Converter<DataPointId, DBObject> {
@@ -24,3 +36,5 @@ public class DataPointIdWriterConverter implements Converter<DataPointId, DBObje
 		return object;
 	}
 }
+
+ */
